daf8d363b0d4a93cd848295c7e9f2725364d3c1b
unknown <laurajquinchia@gmail.com>
2020-11-09 02:29:13 -0500

    Se agrega pylint

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
+"Manejo de imagenes"
 import cv2
 
-def procesamiento():
-    img = cv2.imread("img/animales.jpg")
 
-    height, width = img.shape[0:2]
+class procesamientoImagenes(object):
+    "Implementar casos"
 
-    if height < width and height > 796:
-        orientation = "Vertical"
+    @classmethod
+    def procesamiento(cls):
+        "Se realiza el procesamiento de las imágenes"
+        img = cv2.imread("img/animales.jpg")
 
-        startRow = int(height*.15)
-        startCol = int(width*.15)
-        endRow = int(height*.85)
-        endCol = int(width*.85)
+        height, width = img.shape[0:2]
 
-        croppedImage = img[startRow:endRow, startCol:endCol]
+        return cls.updateimages(img, height, width)
 
-        cv2.imshow('Original Image', img)
-        cv2.imshow('Cropped Image', croppedImage)
-        cv2.waitKey(0)
+    @classmethod
+    def updateimages(cls, img, height, width):
+        "Se realizan las validaciones"
 
-    elif height > width and width > 1123:
-        orientation = "Horizontal"
-        startRow = int(height*.15)
-        startCol = int(width*.15)
-        endRow = int(height*.85)
-        endCol = int(width*.85)
+        start_row = int(height*.15)
+        start_col = int(width*.15)
+        end_row = int(height*.85)
+        end_col = int(width*.85)
 
-        croppedImage = img[startRow:endRow, startCol:endCol]
+        if height < width:
+            orientation = "Vertical"
+            if height > 796:
+                cropped_image = img[start_row:end_row, start_col:end_col]
 
-        cv2.imshow('Original Image', img)
-        cv2.imshow('Cropped Image', croppedImage)
-        cv2.waitKey(0)
-    
-    print(orientation, croppedImage.shape[0:2])
+                cv2.imshow('Cropped Image', cropped_image)
+                cv2.waitKey(0)
 
-if __name__ == '__main__':
-    procesamiento()
+        elif height > width:
+            orientation = "Horizontal"
+            if width > 1123:
+                cropped_image = img[start_row:end_row, start_col:end_col]
+
+                cv2.imshow('Cropped Image', cropped_image)
+                cv2.waitKey(0)
+
+        elif height <= 796 and width > 1123:
+            new_img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
+            cv2.imshow('Resized Image', new_img)
+            cv2.waitKey(0)
+
+        print(orientation, cropped_image.shape[0:2])
 
