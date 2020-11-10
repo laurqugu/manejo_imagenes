89e7cb0e5b896604c0d7c4e5396fd39bcbb2ffd3
unknown <laurajquinchia@gmail.com>
2020-11-09 20:01:43 -0500

    Modificación de imagenes

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class ProcesamientoImagenes(object):
     "Implementar casos"
 
     @classmethod
-    def procesamiento(cls):
+    def procesamiento(cls, image:str):
         "Se realiza el procesamiento de las imágenes"
-        img = cv2.imread("img/animales.jpg")
+        img = cv2.imread(f"uploads/{image}")
 
         height, width = img.shape[0:2]
 
@@ -17,30 +17,40 @@ class ProcesamientoImagenes(object):
     def updateimages(cls, img, height, width):
         "Se realizan las validaciones"
 
+        info = [
+           { 
+               'Ancho Original': height,
+               'Alto Original': width
+            }
+        ]
+
         start_row = int(height*.15)
         start_col = int(width*.15)
         end_row = int(height*.85)
         end_col = int(width*.85)
 
-        if height < width:
-            orientation = "Vertical"
-            if height > 796:
-                cropped_image = img[start_row:end_row, start_col:end_col]
+        if height > 796 and width >= 1123:
+            new_image = img[start_row:end_row, start_col:end_col]
 
-                cv2.imshow('Cropped Image', cropped_image)
-                cv2.waitKey(0)
+            if height < width:
+                orientacion = "Vertical"
+            else:
+                orientacion = "Horizontal"
+        else:
+            print("Redimensionar")
+            new_image = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
 
-        elif height > width:
-            orientation = "Horizontal"
-            if width > 1123:
-                cropped_image = img[start_row:end_row, start_col:end_col]
+            if height < width:
+                orientacion = "Vertical"
+            else:
+                orientacion = "Horizontal"
+        
+        new_med = new_image.shape[0:2]
+        
+        info.append({'nuevo Ancho': new_med[0], 'nuevo alto': new_med[1], 'orientacion': orientacion})
 
-                cv2.imshow('Cropped Image', cropped_image)
-                cv2.waitKey(0)
 
-        elif height <= 796 and width > 1123:
-            new_img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
-            cv2.imshow('Resized Image', new_img)
-            cv2.waitKey(0)
+        print(info)
 
-        print(orientation, cropped_image.shape[0:2])
+if __name__ == '__main__':
+    ProcesamientoImagenes.procesamiento()
